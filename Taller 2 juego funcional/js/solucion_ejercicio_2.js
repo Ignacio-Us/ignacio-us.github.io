34e0a043cc7fa2a7e32936366c898a511dc1edbf
Ignacio-Us <i.uribe01@ufromail.cl>
2024-04-25 19:04:54 -0400

    Avances de Juego
    Se configuro los controles de movimiento para ambos jugadores, funcionan simultaneamente.
    
    Funcionamiento de la barra de salud.
    
    Mensaje de victoria.
    
    Funciones de testeo.
    
    Se hizo el enlace de los objetos con las barras de vida y el sprite del personaje.

--- Taller 2 juego funcional/js/solucion_ejercicio_2.js
+++ Taller 2 juego funcional/js/solucion_ejercicio_2.js
@@ -2,26 +2,31 @@
 
 //Objeto base para los personajes
 class Character {
-  constructor(name, health) {
+  constructor(name, health, spriteId, healthBarId, x, y) {
     //Atributos
     this.name = name;
     this.health = health;
-    this.maxhealth = health;
+    this.maxhealth = 100;
+    this.sprite = document.getElementById(spriteId);
+    this.healthBar = document.getElementById(healthBarId);
+    this.x = x;
+    this.y = y;
   }
+
   //Verifica si el personaje esta vivo
-  isAlive() {
-    return this.health > 0;
+  isDefeat() {
+    return this.health <= 0;
   }
 
   //Ataca a otro personaje seleccionado
   attack(target) {
-    let damage = this.rgpa();
+    let damage = this.rapg();
     console.log(`${this.name} deals ${damage} DMG to ${target.name}`);
     target.health -= damage;
   }
-
-  rgpa() {
-    return Math.round(Math.random() * 6 + 5);
+  //Random Attack Point Generator
+  rapg() {
+    return Math.round(Math.random() * 4 + 1);
   }
 
   //Retorna la información actual del personaje
@@ -30,9 +35,18 @@ class Character {
   }
 
   updateHealthBar() {
-    const healthPercentage = (this.health / 100) * 100;
-    const healthBarCharacter = document.getElementById("health-bar-character");
-    healthBarCharacter.style.width = healthPercentage + "%";
+    let healthPercentage = (this.health / 100) * 100;
+
+    if (healthPercentage < 0) {
+      healthPercentage = 0;
+    }
+
+    this.healthBar.style.width = healthPercentage + "%";
+  }
+
+  updatePosition() {
+    this.sprite.style.left = this.x + "px";
+    this.sprite.style.top = this.y + "px";
   }
 }
 
@@ -49,27 +63,46 @@ function startFight(firstCharacter, secondCharacter) {
 
 function secuenceFight(firstCharacter, secondCharacter) {
   //Primer personaje ataca si esta vivo
-  if (firstCharacter.isAlive()) {
-    firstCharacter.attack(secondCharacter);
-    console.log(hero.status());
-    console.log(juglar.status());
+  if (firstCharacter.isDefeat()) {
+    alert(secondCharacter.name + " Win!");
   } else {
-    console.log(`${firstCharacter.name} died!`);
+    firstCharacter.attack(secondCharacter);
+    secondCharacter.updateHealthBar();
+    console.log(firstCharacter.status());
+    console.log(secondCharacter.status());
   }
 
   //Segundo personaje ataca si esta vivo
-  if (secondCharacter.isAlive()) {
-    secondCharacter.attack(firstCharacter);
-    console.log(hero.status());
-    console.log(juglar.status());
+  if (secondCharacter.isDefeat()) {
+    alert(firstCharacter.name + " Win!");
   } else {
-    console.log(`${secondCharacter.name} died!`);
+    secondCharacter.attack(firstCharacter);
+    firstCharacter.updateHealthBar();
+    console.log(firstCharacter.status());
+    console.log(secondCharacter.status());
   }
 }
 
 //Creación de personajes
-const hero = new Character("Heroe", Math.round(Math.random() * 100) + 1);
-const juglar = new Character("Juglar", Math.round(Math.random() * 100) + 1);
+
+// PLAYER ONE
+const hero = new Character(
+  "Hero",
+  100,
+  "player",
+  "health-bar-player-one",
+  50,
+  300
+);
+// PLAYER TWO
+const juglar = new Character(
+  "Juglar",
+  100,
+  "playerTwo",
+  "health-bar-player-two",
+  1250,
+  300
+);
 
 // Función para detectar colisiones entre los personajes
 function detectarColision(heroe, juglar) {
@@ -83,75 +116,116 @@ function detectarColision(heroe, juglar) {
 
 // Función para manejar la colisión y comenzar la pelea
 function handleCollision() {
+  console.log(detectarColision(hero, juglar));
   if (detectarColision(hero, juglar)) {
     secuenceFight(hero, juglar);
   }
 }
 
-window.onload = startFight(hero, juglar);
-
-// PLAYER ONE
-
-const $player = document.getElementById("player");
-let x = 0;
-let y = -250;
+// Variables para mantener el estado de las teclas presionadas para cada jugador
+const player1KeysPressed = {};
+const player2KeysPressed = {};
 
 document.addEventListener("keydown", (e) => {
-  console.log(e.code);
-  //Flecha hacia Arriba
-  if (e.code === "ArrowUp") {
-    y = y + 50;
-    $player.style.top = -y + "px";
-  }
+  const code = e.code;
 
-  //Flecha hacia Abajo
-  if (e.code === "ArrowDown") {
-    y = y - 50;
-    $player.style.top = -y + "px";
+  // Marcar la tecla como presionada para el jugador correspondiente
+  if (
+    code === "ArrowUp" ||
+    code === "ArrowDown" ||
+    code === "ArrowLeft" ||
+    code === "ArrowRight"
+  ) {
+    player1KeysPressed[code] = true;
   }
-
-  //Flecha hacia la Izquierda
-  if (e.code === "ArrowLeft") {
-    x = x - 50;
-    $player.style.left = x + "px";
-  }
-
-  //Flecha hacia la Derecha
-  if (e.code === "ArrowRight") {
-    x = x + 50;
-    $player.style.left = x + "px";
+  if (
+    code === "KeyW" ||
+    code === "KeyS" ||
+    code === "KeyA" ||
+    code === "KeyD"
+  ) {
+    player2KeysPressed[code] = true;
   }
 });
 
-// PLAYER TWO
+document.addEventListener("keyup", (e) => {
+  const code = e.code;
 
-const $playerTwo = document.getElementById("playerTwo");
-let x2 = 900;
-let y2 = -250;
+  // Marcar la tecla como liberada para el jugador correspondiente
+  if (
+    code === "ArrowUp" ||
+    code === "ArrowDown" ||
+    code === "ArrowLeft" ||
+    code === "ArrowRight"
+  ) {
+    delete player1KeysPressed[code];
+  }
+  if (
+    code === "KeyW" ||
+    code === "KeyS" ||
+    code === "KeyA" ||
+    code === "KeyD"
+  ) {
+    delete player2KeysPressed[code];
+  }
+});
 
-document.addEventListener("keydown", (k) => {
-  console.log(k.code);
-  //Tecla W
-  if (k.code === "KeyW") {
-    y2 = y2 + 50;
-    $playerTwo.style.top = -y2 + "px";
+// Función para mover a los jugadores
+function movePlayers() {
+  // Movimientos del jugador 1
+  if ("ArrowUp" in player1KeysPressed) {
+    hero.y -= 25;
+    hero.updatePosition();
+  }
+  if ("ArrowDown" in player1KeysPressed) {
+    hero.y += 25;
+    hero.updatePosition();
+  }
+  if ("ArrowLeft" in player1KeysPressed) {
+    hero.x -= 25;
+    hero.updatePosition();
+  }
+  if ("ArrowRight" in player1KeysPressed) {
+    hero.x += 25;
+    hero.updatePosition();
   }
 
-  //Tecla S
-  if (k.code === "KeyS") {
-    y2 = y2 - 50;
-    $playerTwo.style.top = -y2 + "px";
+  // Movimientos del jugador 2
+  if ("KeyW" in player2KeysPressed) {
+    juglar.y -= 25;
+    juglar.updatePosition();
   }
-
-  //Tecla A
-  if (k.code === "KeyA") {
-    x2 = x2 - 50;
-    $playerTwo.style.left = x2 + "px";
+  if ("KeyS" in player2KeysPressed) {
+    juglar.y += 25;
+    juglar.updatePosition();
   }
+  if ("KeyA" in player2KeysPressed) {
+    juglar.x -= 25;
+    juglar.updatePosition();
+  }
+  if ("KeyD" in player2KeysPressed) {
+    juglar.x += 25;
+    juglar.updatePosition();
+  }
+}
 
-  //Tecla D
-  if (k.code === "KeyD") {
-    x2 = x2 + 50;
-    $playerTwo.style.left = x2 + "px";
+// Sirve para simular un ataque y comprobar si la barra de vida funciona correctamente
+function simulateAttack() {
+  hero.health -= 99;
+  hero.updateHealthBar();
+}
+
+// Ventana que muestra El comienzo de la pelea
+window.onload = startFight(hero, juglar);
+
+// Llamar a la función movePlayers en un bucle de animación o en un temporizador
+setInterval(movePlayers, 1000 / 30);
+hero.updateHealthBar();
+
+document.addEventListener("keydown", (e) => {
+  const code = e.code;
+
+  if (code === "KeyM") {
+    secuenceFight(hero, juglar);
   }
 });
